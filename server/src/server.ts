16c726fed7af8e6bc4109db96df715fb4f353a32
Elaine Muhombe <emuhombe@gmail.com>
2024-10-20 20:16:46 +0300

    feat: setup server and database connection with Mongoose

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,27 @@
 //src/server.ts
+import { configDotenv } from "dotenv";
+import express, { Request, Response } from "express";
+import mongoose from "mongoose";
+
+// Load environment variables
+configDotenv();
+
+// Create an instance of express
+const app = express();
+
+const port = 5300;
+const uri = process.env.MONGODB_URL;
+
+// Database connection
+mongoose.connect(uri as string)
+    .then(() => {
+        console.log("Database connection successful");
+        // Start the server once database connection is successful
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        });
+    })
+    .catch((error) => {
+        console.error("Database connection failed: ", error);
+    });
+
