329c48634921e1758cb81e51f214b56e07c2349d
Elaine Muhombe <emuhombe@gmail.com>
2024-10-20 22:46:27 +0300

    Refactor types in types.ts: Updated interfaces for user and marks with Mongoose Document integration and consistent naming conventions

--- server/src/types/types.ts
+++ server/src/types/types.ts
@@ -1,7 +1,8 @@
 // src/types/types.ts
+import { Document } from 'mongoose';
 
 // Interface representing a user in the system
-interface User {
+export default interface IUser {
     name: string;            // The name of the user
     email: string;           // The email address of the user
     password: string;        // The password for user authentication
@@ -9,35 +10,35 @@ interface User {
 }
 
 // Base interface for different types of marks
-interface BaseMark {
+export  interface IBaseMark extends Document {
     studentId: string;      // Unique identifier for the student
     marks: number;           // Marks received by the student
     date: Date;              // Date of the marks entry
 }
 
 // Interface for attendance marks, extending BaseMark
-interface AttendanceMark extends BaseMark {
+export  interface IAttendanceMark extends IBaseMark {
     attendancePercentage: number; // Percentage of attendance
     status: 'present' | 'absent';  // Attendance status of the student
 }
 
 // Interface for project review marks, extending BaseMark
-interface ProjectReviewMark extends BaseMark {
+export interface IProjectReviewMark extends IBaseMark {
     feedback: string;         // Feedback provided for the project review
 }
 
 // Interface for LinkedIn post marks, extending BaseMark
-interface LinkedInPostMark extends BaseMark { 
+export interface ILinkedInPostMark extends IBaseMark { 
     postLink: string;        // Link to the student's LinkedIn post
 }
 
 // Interface for Assessment marks, extending BaseMark
-interface AssessmentMark extends BaseMark{
+export interface IAssessmentMark extends IBaseMark{
     assessmentTitle: string // Title of the assessment
 }
 
 // Interface representing total marks for a student
-interface TotalMarks {
+export interface ITotalMarks {
     studentId: string;             // Unique identifier for the student
     totalAttendanceMarks: number;   // Total attendance marks
     totalProjectReviewMarks: number; // Total project review marks
