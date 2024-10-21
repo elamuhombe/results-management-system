67b2b15c30b55b0311cc5b132293fe219a14ac7f
Elaine Muhombe <emuhombe@gmail.com>
2024-10-21 19:23:15 +0300

    chore: export SessionModel

---
+++ server/src/models/sessionModel.ts
@@ -0,0 +1,14 @@
+import { model, Schema } from "mongoose";
+import { ISession } from "../types/types";
+
+const sessionSchema = new Schema<ISession>({
+    userId:{type: Schema.Types.ObjectId, required: true},
+    expiresAt:{type: Date},
+    ipAddress: {type: String, required: true},
+    userAgent:{type: String, required: true},
+    isActive:{type: Boolean,required: true},
+
+},{timestamps: true})
+
+// export the model
+export const SessionModel = model<ISession>("sessionModel", sessionSchema)
