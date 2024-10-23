b6bf8d5e263222d8e9ea2e6c426094862a1d2db2
Elaine Muhombe <emuhombe@gmail.com>
2024-10-23 17:56:03 +0300

    implement AttendanceMarkService for managing attendance marks
    - Add methods to create, retrieve, update, and delete attendance marks.
    - Integrate AttendanceMarkRepository for database operations.
    - Implement type safety using IAttendanceMark interface.

---
+++ server/src/services/attendanceMarksService.ts
@@ -0,0 +1,39 @@
+//src/services/attendanceMarksService
+
+import AttendanceMarkRepository from '../repositories/marks/attendanceRepository';
+import { IAttendanceMark } from '../types/types';
+
+class AttendanceMarkService {
+    private attendanceMarkRepository: AttendanceMarkRepository;
+
+    constructor() {
+        this.attendanceMarkRepository = new AttendanceMarkRepository();
+    }
+
+    // Service method to create a new attendance mark
+    async createAttendanceMark(attendanceData: IAttendanceMark): Promise<IAttendanceMark> {
+        return await this.attendanceMarkRepository.createAttendanceMark(attendanceData);
+    }
+
+    // Service method to get all attendance marks
+    async getAllAttendanceMarks(): Promise<IAttendanceMark[]> {
+        return await this.attendanceMarkRepository.getAllAttendanceMarks();
+    }
+
+    // Service method to get a specific attendance mark by student ID
+    async getAttendanceMarkByStudentId(studentId: string): Promise<IAttendanceMark | null> {
+        return await this.attendanceMarkRepository.getAttendanceMarkByStudentId(studentId);
+    }
+
+    // Service method to update attendance mark
+    async updateAttendanceMark(studentId: string, updates: Partial<IAttendanceMark>): Promise<IAttendanceMark | null> {
+        return await this.attendanceMarkRepository.updateAttendanceDataByStudentId(studentId)
+    }
+
+    // Service method to delete an attendance mark
+    async deleteAttendanceMark(studentId: string): Promise<IAttendanceMark | null> {
+        return await this.attendanceMarkRepository.deleteAttendanceMark(studentId);
+    }
+}
+
+export default AttendanceMarkService;
