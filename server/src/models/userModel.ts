31bd0610d7aa2429a8959ced7b0d74ef098b6fcc
Elaine Muhombe <emuhombe@gmail.com>
2024-10-23 18:01:08 +0300

    chore: update User model to include userId and conditional studentId fields
    - Add userId field for user identification.
    - Include studentId field, required only for users with the 'student' role.
    - Adjuste comments for clarity.

--- server/src/models/userModel.ts
+++ server/src/models/userModel.ts
@@ -5,7 +5,10 @@ import IUser from "../types/types";
 // User schema definition
 const userSchema = new Schema<IUser>({
     _id: { type: Schema.Types.ObjectId, required: true, auto: true },
-    name: { type: String, required: true },                      // The name of the user
+    name: { type: String, required: true },
+    userId: { type: String, required: true },                      // The name of the user
+    studentId: { type: String, required: function() 
+        { return this.userRole === 'student'; } }, // Required for 'student' role
     email: { type: String, required: true, unique: true },      // The email address of the user
     password: { type: String, required: true },                 // The password for user authentication
     userRole: { type: String, enum: ['admin', 'student'], required: true } // The role of the user
