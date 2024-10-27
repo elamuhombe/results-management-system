408ad012bf815a7e7010837c9f9374787cb4d896
Elaine Muhombe <emuhombe@gmail.com>
2024-10-27 20:54:37 +0300

    feat(models): enhance marks models with new schemas and properties

--- server/src/models/marksModel.ts
+++ server/src/models/marksModel.ts
@@ -1,55 +1,103 @@
 //src/models/marksModel.ts
-import  { model, Schema } from "mongoose";
-import { IBaseMark, IAttendanceMark, IProjectReviewMark, ILinkedInPostMark, IAssessmentMark } from "../types/types";
-
+import { model, Schema } from "mongoose";
+import {
+  IBaseMark,
+  IAttendanceMark,
+  IProjectReviewMark,
+  ILinkedInPostMark,
+  IAssessmentMark,
+  IProjectSubmissionMark,
+} from "../types/types";
+import { timeStamp } from "console";
 
 // BaseMark schema
 const BaseMarkSchema = new Schema<IBaseMark>({
-    student: {
-        _id: { type: Schema.Types.ObjectId, required: true },
-        name: { type: String, required: true }, // Student name
-        userRole: { type: String, required: true, enum: ['student'] }, // Only 'student' role
-        studentId: { type: String, required: true }, // Student ID
-    },
-   
-    marks: { type: Number, required: true, min: 0 }, // Marks received by the student
-    date: { type: Date, required: true }, // Date of the marks entry
+  student: {
+    _id: { type: Schema.Types.ObjectId, required: true },
+    name: { type: String, required: true }, // Student name
+    studentId: { type: String, required: true }, // Student ID
+  },
+
+  marks: { type: Number, required: true, min: 0 }, // Marks received by the student
+  date: { type: Date, required: true }, // Date of the marks entry
 });
 
 // AttendanceMark schema
-const AttendanceMarkSchema = new Schema<IAttendanceMark>({}, { timestamps: true });
+const AttendanceMarkSchema = new Schema<IAttendanceMark>(
+  {},
+  { timestamps: true }
+);
 
 AttendanceMarkSchema.add({
-    ...BaseMarkSchema.obj,
-    _id: { type: Schema.Types.ObjectId, required: true, auto: true },
-    attendancePercentage: { type: Number, required: true, min: 0, max: 100 },
-    status: { type: String, enum: ['present', 'absent'], required: true },
-}, );
+  ...BaseMarkSchema.obj,
+  _id: { type: Schema.Types.ObjectId, required: true, auto: true },
+  attendancePercentage: { type: Number, required: true, min: 0, max: 100 },
+  status: { type: String, enum: ["present", "absent"], required: true },
+});
 
 // ProjectReviewMark schema
-const ProjectReviewMarkSchema = new Schema<IProjectReviewMark>({}, { timestamps: true });
+const ProjectReviewMarkSchema = new Schema<IProjectReviewMark>(
+  {},
+  { timestamps: true }
+);
 ProjectReviewMarkSchema.add({
-    ...BaseMarkSchema.obj,
-    _id: { type: Schema.Types.ObjectId, required: true, auto: true },
-    feedback: { type: String, required: true },
+  ...BaseMarkSchema.obj,
+  _id: { type: Schema.Types.ObjectId, required: true, auto: true },
+  project_title: { type: String, required: true }, 
+  feedback: { type: String, required: true },
+})
+
+// ProjectSubmissionMark schema
+const ProjectSubmissionMarkSchema = new Schema<IProjectSubmissionMark>(
+  {},
+  { timestamps: true }
+);
+ProjectSubmissionMarkSchema.add({
+  ...BaseMarkSchema.obj,
+  _id: { type: Schema.Types.ObjectId, required: true, auto: true },
+  project_title: { type: String, required: true },
 });
 
 // LinkedInPostMark schema
-const LinkedInPostMarkSchema = new Schema<ILinkedInPostMark>({}, { timestamps: true });
+const LinkedInPostMarkSchema = new Schema<ILinkedInPostMark>(
+  {},
+  { timestamps: true }
+);
 LinkedInPostMarkSchema.add({
-    ...BaseMarkSchema.obj,
-    postLink: { type: String, required: true },
+  ...BaseMarkSchema.obj,
+  postLink: { type: String, required: true },
 });
 
 // AssessmentMark schema
-const AssessmentMarkSchema = new Schema<IAssessmentMark>({}, { timestamps: true });
+const AssessmentMarkSchema = new Schema<IAssessmentMark>(
+  {},
+  { timestamps: true }
+);
 AssessmentMarkSchema.add({
-    ...BaseMarkSchema.obj,
-    assessmentTitle: { type: String, required: true },
+  ...BaseMarkSchema.obj,
+  _id: { type: Schema.Types.ObjectId, required: true, auto: true },
+  subject: { type: String, required: true },
 });
 
 // Export the models
-export const AttendanceMarkModel = model<IAttendanceMark>('AttendanceMark', AttendanceMarkSchema);
-export const ProjectReviewMarkModel = model<IProjectReviewMark>('ProjectReviewMark', ProjectReviewMarkSchema);
-export const LinkedInPostMarkModel = model<ILinkedInPostMark>('LinkedInPostMark', LinkedInPostMarkSchema);
-export const AssessmentMarkModel = model<IAssessmentMark>('AssessmentMark', AssessmentMarkSchema);
+export const AttendanceMarkModel = model<IAttendanceMark>(
+  "AttendanceMark",
+  AttendanceMarkSchema
+);
+export const ProjectReviewMarkModel = model<IProjectReviewMark>(
+  "ProjectReviewMark",
+  ProjectReviewMarkSchema
+);
+export const ProjectSubmissionMarkModel = model<IProjectSubmissionMark>(
+  "ProjectSubmissionMark",
+  ProjectSubmissionMarkSchema
+);
+
+export const LinkedInPostMarkModel = model<ILinkedInPostMark>(
+  "LinkedInPostMark",
+  LinkedInPostMarkSchema
+);
+export const AssessmentMarkModel = model<IAssessmentMark>(
+  "AssessmentMark",
+  AssessmentMarkSchema
+);
