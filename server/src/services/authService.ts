5ca33641484d2ad68dec69806697d86805091d50
Elaine Muhombe <emuhombe@gmail.com>
2024-10-27 20:42:31 +0300

    feat(authService): implement user login and logout functionalities

--- server/src/services/authService.ts
+++ server/src/services/authService.ts
@@ -1,1 +1,24 @@
 //src/services/authService.ts
+
+import authRepository from '../repositories/authRepository';
+import IUser from '../types/types';
+
+class AuthService {
+    // Login a user by verifying credentials and generating a token
+    async login(email: string, password: string): Promise<{ token: string; user: IUser }> {
+        try {
+            // Call the repository to handle user login logic
+            const result = await authRepository.loginUser(email, password);
+            return result;
+        } catch (error: any) {
+            throw new Error(error.message);
+        }
+    }
+
+    // Logout functionality
+    logout(): void {
+        authRepository.logoutUser();
+    }
+}
+
+export default new AuthService();
