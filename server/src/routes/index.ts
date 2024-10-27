26d9e21456b3bd77eb3a4f28896bda63b47099fc
Elaine Muhombe <emuhombe@gmail.com>
2024-10-27 21:10:12 +0300

    feat: consolidate route exports in index.ts

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,1 +1,9 @@
 //src/routes/index.ts
+
+export { default as authRoute } from "./userRoute";
+export { default as assessmentRoutes } from "./assessmentMarksRoute";
+export { default as userRoutes } from "./userRoute";
+export { default as attendanceRoutes } from "./attendanceMarksRoute";
+export { default as linkedinRoutes } from "./linkedinPostMarksRoute";
+export { default as projectReviewRoutes } from "./projectReviewMarksRoute";
+export { default as projectSubmissionRoutes } from "./projectSubmissionMarksRoute";
