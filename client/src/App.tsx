6c0b1261c92579f10f13578f39230d1ec8acb251
Elaine Muhombe <emuhombe@gmail.com>
2024-11-02 19:26:51 +0300

    feat: implement routing in App component with authentication context
    - Integrate  to manage routing within the application
    - Set up routes for Login, Forgot Password, Admin Dashboard, Student Dashboard, and a Not Found page
    - Wrappe routes in  to provide authentication context to all components
    - Remove previous login form structure in favor of a structured routing approach

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,25 @@
-//src/App.tsx
-import { LoginForm } from './components'
-import './index.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import { AuthProvider } from "./context/AuthContext";
+import "./index.css";
+
+import NotFound from "./components/NotFound";
+import { AdminDashboard, ForgotPasswordForm, Login, StudentDashboard } from "./components";
 
 function App() {
-
-
   return (
-    <>
-      <div>
-        {/* user login form */}
-        <LoginForm />
-      </div>
-     
-    </>
-  )
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Login />} /> {/* Login route */}
+        <Route path  ="/reset-password" element={<ForgotPasswordForm />} />{" "}
+        <Route path="/admin/dashboard" element={<AdminDashboard />} />{" "}
+        {/* Admin dashboard route */}
+        <Route path="/student/dashboard" element={<StudentDashboard />} />{" "}
+        {/* Student dashboard route */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
